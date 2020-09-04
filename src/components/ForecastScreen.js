d06bb0ae159ff2119982d2ecf10cda0f83efbd05
Poornima G <55700190+poorni18@users.noreply.github.com>
2020-09-04 22:23:23 +0530

    Update ForecastScreen.js

--- src/components/ForecastScreen.js
+++ src/components/ForecastScreen.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { WeatherContext } from '../context/WeatherContext'
+
 
 const ForeCastScreen = () => {
+
+    const { data } = React.useContext(WeatherContext);
+    console.log(data.weatherData);
+
+    const foreCastObj = data.weatherData[data.selectedState].cities.find(obj => obj.name === data.selectedCity).forecast;
+
     return <div>
-        <div>This is the Forecast screen. Please find the details below Chicago IL</div>
-        
+        <div className="header">
+            <h2>{`Five days Forecast for ${data.selectedCity}, ${data.selectedState} `}</h2>
         </div>
+        {
+            foreCastObj.map(forecast => {
+                return <div>
+                    <div className="container">
+                        <h3 className="day">{forecast.Date}</h3>
+                        <h3 className="time">{forecast.Time}</h3>
+                        <div className="partlycloudy">
+                            <p className="conditions">Partly Cloudy</p>
+                            <p className="tempRange"><span className="high">{forecast.temprature}</span></p>
+                            <p className="feelsLike">Feels Like: <span className="high">{forecast.feels}</span></p>
+                        </div>
+                    </div>
+                </div>
+            })
+        }
+    </div>
 }
 
 export default ForeCastScreen;
